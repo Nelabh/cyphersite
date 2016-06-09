ce726fa6fa980a71a00dc5c43e6265dafcfa75b6
Sushumna Khandelwal <sush.94kh@gmail.com>
2016-06-09 14:15:43 +0530

    ..

--- contact/index.php
+++ contact/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <?php include '../header.php' ; ?>
  <link rel="stylesheet" href="a.css" />
+ <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <body class="size-1140">
   <!-- TOP NAV WITH LOGO -->
   <?php include '../menu.php' ; ?>
@@ -175,6 +176,7 @@
 </section>
 <?php include '../footer.php' ; ?>
  <script src="a.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
      
    </body>
