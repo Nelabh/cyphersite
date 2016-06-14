eb3c7013168179b25f9554af45ee192dd0a6d51b
Sushumna Khandelwal <sush.94kh@gmail.com>
2016-06-14 14:13:34 +0530

    .

--- our-work/index.php
+++ our-work/index.php
@@ -3,125 +3,126 @@
 
 
 <?php include '../header.php' ; ?>
+  <!-- ===========================
+    STYLESHEETS
+    =========================== -->    
+     <link rel="icon" href="img/favicon.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-ipad-retina.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-iphone-retina.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-ipad.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="img/apple-touch-icon-iphone.png" />
+     
+    <!-- ===========================
+    STYLESHEETS
+    =========================== -->    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/preloader.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/animate.css">
+        
+    <!-- ===========================
+    ICONS: 
+    =========================== -->
+    <link rel="stylesheet" href="css/simple-line-icons.css">    
+    
+    <!-- ===========================
+    GOOGLE FONTS
+    =========================== -->    
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Antic|Raleway:300">
+   
 
 <body class="size-1140">
   <!-- TOP NAV WITH LOGO -->
   <?php include '../menu.php' ; ?>
-<section>
-  <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
 
-<!-- font awesome -->
-<link href="assets/css/font-awesome.min.css" rel="stylesheet">
-<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
-
-<!-- animate.css -->
-<link rel="stylesheet" href="assets/animate/animate.css" />
-<link rel="stylesheet" href="assets/animate/set.css" />
-
-<!-- gallery -->
-<link rel="stylesheet" href="assets/gallery/blueimp-gallery.min.css">
-
-<!-- favicon -->
-<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+         <div class="container">
+            <div class="line" >
+             <div class="sectionhead  row wow fadeInUp">
+            <span class="bigicon icon-cup "></span>
+            <h1>This is what We can do for you</h1>
+         </div><!--SERVICE SECTION HEADING END-->
 
 
-<link rel="stylesheet" href="assets/style.css">
-
-
-
-<script src="assets/js/jquery-1.11.1.js"></script>
-
-<script src="assets/js/table.js"></script>
-
-<script src="assets/js/search.js"></script>
-
-<link href="assets/css/bootstrap.css" rel="stylesheet" />
-<!-- ION ICONS STYLES -->
-<link href="assets/css/ionicons.css" rel="stylesheet" />
-<!-- FONT AWESOME ICONS STYLES -->
-<link href="assets/css/font-awesome.css" rel="stylesheet" />
-<!-- FANCYBOX POPUP STYLES -->
-<link href="assets/js/source/jquery.fancybox.css" rel="stylesheet" />
-<!-- STYLES FOR VIEWPORT ANIMATION -->
-<link href="assets/css/animations.min.css" rel="stylesheet" />
-
-         <div id="our-work">
-            <div class="line" style="padding-top: 3%">
-               <h2 class="section-title">Our Work</h2>
             
-               <div id="works"  class=" clearfix grid"> 
-    <figure class="effect-oscar  wowload fadeIn">
-        <img src="images/portfolio/11.jpg" alt="img01"/>
-        <figcaption>
-            <h2>PAYROLL</h2>
-            <p><br>
-            <a href="{{URL::asset('/information')}}" title="1" data-gallery></a></p>            
-        </figcaption>
-    </figure>
-     <figure class="effect-oscar  wowload fadeInUp">
-        <img src="images/portfolio/12.png" alt="img01"/>
-        <figcaption>
-            <h2>ERP</h2>
-            <a href="images/portfolio/12.png" title="1" data-gallery></a></p>            
-        </figcaption>
-    </figure>
     
-     <figure class="effect-oscar  wowload fadeInUp">
-        <img src="images/portfolio/14.png" alt="img01"/>
-        <figcaption>
-            <h2>MAGENTO EXTENSIONS</h2>
-            <a href="images/portfolio/14.png" title="1" data-gallery></a></p>            
-        </figcaption>
-    </figure>
-     <figure class="effect-oscar  wowload fadeInUp">
-        <img src="images/portfolio/15.jpg" alt="img01"/>
-        <figcaption>
-            <h2>WEB APPLICATION</h2>
-            <a href="{{URL::asset('/railway')}}" title="1" data-gallery></a></p>            
-        </figcaption>
-    </figure>
-     
-     <figure class="effect-oscar  wowload fadeInUp">
-        <img src="images/portfolio/16.jpg" alt="img01"/>
-        <figcaption>
-            <h2>DESKTOP APPLICATION</h2>
-            <a href={{URL::asset('/pincode')}} title="1" data-gallery></a></p>            
-        </figcaption>
-    </figure>
-    <figure class="effect-oscar  wowload fadeInUp">
-        <img src="images/portfolio/17.jpg" alt="img01"/>
-        <figcaption>
-            <h2>BIOMETRIC INTEGRAPTION</h2>
-            <a href="{{URL::asset('/wholesale')}}" title="1" data-gallery></a></p>            
-        </figcaption>
-    </figure>
-    <figure class="effect-oscar  wowload fadeInUp">
-        <img src="images/portfolio/18.jpg" alt="img01"/>
-        <figcaption>
-            <h2>INVENTORY MANAGEMENT</h2>
-            <a href="{{URL::asset('/information')}}" title="1" data-gallery></a></p>            
-        </figcaption>
-    </figure>
-    <figure class="effect-oscar  wowload fadeInUp">
-        <img src="images/portfolio/19.png" alt="img01"/>
-        <figcaption>
-            <h2>CRM</h2>
-            <a href="{{URL::asset('/information')}}" title="1" data-gallery></a></p>            
-        </figcaption>
-    </figure>
-   
-    
-   </div>
+               <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-duration="3s">
+               	<center>
+                   <img src="img/s1.png" alt="">
+                   <h4>Payroll</h4>
+                   <p>payroll management system as a software and service for company / business salary accounting</p></center>
+                </div> <!-- ITEM END -->
 
+                <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-duration="3s">
+                <center>
+                   <img src="img/s2.png" alt="">
+                   <h4>ERP</h4>
+                   <p>:  ERP integrates varied organizational systems and facilitates error-free transactions and production</p></center>
+                </div> <!-- ITEM END -->
 
+                <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-duration="3s">
+                <center>
+                   <img src="img/s3.jpg" alt="">
+                   <h4>Windows Standalone Application</h4>
+                   <p>Provides standalone applications for various domains
+Web:  Includes work related to  developing a web site for the Internet 
+/p></center>
+                </div> <!-- ITEM END -->
 
+               <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-duration="3s">
+                <center>
+                   <img src="img/s4.gif" alt="">
+                   <h4>Biometric Integration</h4>
+                   <p>Exceeding the needs of the most demanding fingerprint enrollment and verification environments.</p></center>
+                </div> <!-- ITEM END -->
+
+               
+
+                <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-duration="3s">
+                <center>
+                   <img src="img/s6.jpg" alt="">
+                   <h4>Web Development</h4>
+                   <p>Develop websites for all fields to optimize your work and enhance your business.</p></center>
+                </div> <!-- ITEM END -->
+                <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-duration="3s">
+                <center>
+                   <img src="img/s7.png" alt="">
+                   <h4>CRM</h4>
+                   <p>:: CRM software for managing your sales, marketing, customer support, and inventory in a single system</p></center>
+                </div> <!-- ITEM END -->
+                <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-duration="3s">
+                <center>
+                   <img src="img/s8.png" alt="">
+                   <h4>Android App Development</h4>
+                   <p>Android app development is done for all categories in the play store</p></center>
+                </div> <!-- ITEM END -->
+                <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-duration="3s">
+                <center>
+                   <img src="img/s9.png" alt="">
+                   <h4>Magento Extensions</h4>
+                   <p>design top quality professional eCommerce extensions which are beautifully coded, easy to use, and packed with features that will take your Magento® store to the next level</p></center>
+                </div> <!-- ITEM END -->
+                 <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-duration="3s">
+                <center>
+                   <img src="img/s5.jpg" alt=""></center>
+                   <h4>Inventory Management</h4>
+                   <p>Stop wasting time on manual inventory management tasks. Easily control your stock in multiple locations, manage sales and purchase orders in a click, and even automate backordering!</p></center>
+                </div> <!-- ITEM END -->
+        </div><!-- SERVICE ITEMS END-->
             
             </div>
          </div>         
          
-</section>
+
 <?php include '../footer.php' ; ?>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    
+    <!--Other necessary scripts-->
+    <script src="js/jquery.nicescroll.min.js"></script>
+    <script src="js/jquery.jribbble-1.0.1.ugly.js"></script>
+    <script src="js/drifolio.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script>new WOW().init();</script>    
 
      
    </body>
